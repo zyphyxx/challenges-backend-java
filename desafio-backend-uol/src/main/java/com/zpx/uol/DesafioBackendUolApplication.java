2a7d9a5b504f54d14675d40a3769b182405089f5
Edgar Jesus <mendesbr92@gmail.com>
2023-11-15 17:48:59 -0300

    first commit uol

---
+++ desafio-backend-uol/src/main/java/com/zpx/uol/DesafioBackendUolApplication.java
@@ -0,0 +1,13 @@
+package com.zpx.uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendUolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioBackendUolApplication.class, args);
+    }
+
+}
---
+++ desafio-backend-uol/src/test/java/com/zpx/uol/DesafioBackendUolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpx.uol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendUolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
